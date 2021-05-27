96bab281d8635207b42e1136fecbb2114ef3c24f
Venolab <78386082+Venolab@users.noreply.github.com>
2021-05-27 13:18:20 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -73,8 +73,6 @@ function displayTemperature(response) {
    let dateElement = document.querySelector("#date");
    let iconElement = document.querySelector("#icon");
 
-   celsiusTemperature = response.data.main.temp;
-
    temperatureElement.innerHTML = Math.round(response.data.main.temp);
    cityElement.innerHTML = response.data.name;
    descriptionElement.innerHTML = response.data.weather[0].description;
@@ -105,29 +103,13 @@ function handleSubmit(event) {
 function displayfahrenheitTemperature(event) {
    event.preventDefault();
    let temperatureElement = document.querySelector("#temperature");
-   celsiusLink.classList.remove("active");
-   fahrenheitLink.classList.add("active");
-   let FahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-   alert(FahrenheitTemperature);
    temperatureElement.innerHTML = Math.round(FahrenheitTemperature);
 }
-
-function displaycelsiusTemperature(event) {
-   event.preventDefault;
-   celsiusLink.classList.add("active");
-   fahrenheitLink.classList.remove("active");
-   let temperatureElement = document.querySelector("#temperature");
-   temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
-let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayfahrenheitTemperature);
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displaycelsiusTemperature);
 
 search("New York"); 
